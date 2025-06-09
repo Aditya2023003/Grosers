567de78b480aba79981971695f6cb0dd3a9e8e09
Aditya2023003 <143504999+Aditya2023003@users.noreply.github.com>
2025-06-09 20:04:28 +0530

    Add files via upload

---
+++ grosers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grosers
+//
+//  Created by Aditya Raj on 14/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grosers/grosersApp.swift
@@ -0,0 +1,17 @@
+//
+//  grosersApp.swift
+//  grosers
+//
+//  Created by Aditya Raj on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct grosersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
